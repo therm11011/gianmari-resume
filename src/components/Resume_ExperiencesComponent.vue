13f1d53abf9157bcca7d27c21e5eaaac036b0f8e
Gian Mari Ramos <giaaanmari@gmail.com>
2026-02-09 16:54:27 +0800

    added flex-col when sm size

--- src/components/Resume_ExperiencesComponent.vue
+++ src/components/Resume_ExperiencesComponent.vue
@@ -12,11 +12,11 @@ const experiences = useConvexQuery(api.resume.getExperiences);
         <div class="absolute my-10">
             <h1 class="text-xl border rounded-r-full px-6 py-3 bg-white font-extrabold text-sky-700 shadow-[0px_6px_0px_#144272]">EXPERIENCES</h1>
         </div>
-        <div class="h-full flex flex-col gap-12 mx-20">
+        <div class="h-full flex flex-col gap-12">
             <div class="mt-20">
                 
             </div>
-            <div class="flex gap-12 justify-center overflow-hidden">
+            <div class="flex flex-col md:flex-row gap-12 justify-center overflow-hidden">
                 
                 <div v-for="value in experiences.data.value" >
                     <ExperienceCardComponent :experience="value"/>
