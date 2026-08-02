d7c505ea0af4deeda796820fd6af16cef7dbb25a
therm <giaaanmari@gmail.com>
2026-08-02 12:41:54 +0800

    feat: added cloudflare analytics

--- app/app.vue
+++ app/app.vue
@@ -2,9 +2,21 @@
 import AppFooter from './components/layout/appFooter.vue';
 import AppHeader from './components/layout/appHeader.vue';
 import MarkdownLoading from './components/layout/markdownLoading.vue';
+
+useHead({
+  script: [
+    {
+      type: 'module',
+      src: 'https://static.cloudflareinsights.com/beacon.min.js',
+      'data-cf-beacon': JSON.stringify({
+        token: 'd4d7fc5bab83408b8b9c81d22ef5af82',
+      }),
+    },
+  ],
+})
 </script>
 
-<template>
+<template><!-- Cloudflare Web Analytics --><script type='module' src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "d4d7fc5bab83408b8b9c81d22ef5af82"}'></script><!-- End Cloudflare Web Analytics -->
   <MarkdownLoading />
   <AppHeader />
   <NuxtPage />
